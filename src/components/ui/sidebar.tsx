1311cab0092ccd7f2a0175c455c38d99d956fe4a
Vikesh Patel <patelvikesh1002@gmail.com>
2025-11-10 09:51:29 +0000

    look its overlaping

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -324,6 +324,7 @@ const SidebarInset = React.forwardRef<
       ref={ref}
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background",
+        "md:peer-data-[collapsible=icon]:ml-[--sidebar-width-icon]",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
