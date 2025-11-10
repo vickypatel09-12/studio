0f49b21de59d2d6504ca7bb791f1b121c1f753f6
Vikesh Patel <patelvikesh1002@gmail.com>
2025-11-10 13:49:07 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,16 @@
-// This file is used to store your Firebase configuration.
-// It is git-ignored, so it is safe to place your credentials here.
-
-// IMPORTANT: Replace the placeholder "..." values with your actual
-// Firebase project credentials.
+// This file is not intended to be committed to your Git repository.
+// It is recommended to add it to your .gitignore file.
+//
+// To get started, create a new Firebase project and go to the Project Settings
+// page. Under the "General" tab, scroll down to "Your apps" and click on the
+// "Web" icon to create a new web app. Copy the `firebaseConfig` object and
+// paste it here.
 
 export const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
+  apiKey: "AIzaSyATqgD30xUydeiWSK32B-yXr-VL_O0560Y",
+  authDomain: "studio-5073934541-bb30d.firebaseapp.com",
+  projectId: "studio-5073934541-bb30d",
+  storageBucket: "studio-5073934541-bb30d.appspot.com",
+  messagingSenderId: "382799742399",
+  appId: "1:382799742399:web:6648449c7e19b702610d0a"
 };
