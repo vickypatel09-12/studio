80827ec5237da5028cb2cc4cf35c58f7683c1929
Vikesh Patel <patelvikesh1002@gmail.com>
2025-11-10 10:33:42 +0000

    Error occurred prerendering page "/_not-found". Read more: https://nextj

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  skipMiddlewareUrlNormalize: true,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
